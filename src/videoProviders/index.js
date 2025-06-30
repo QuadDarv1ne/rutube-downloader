9394daccf500cdb78fc015888a19d7af00357961
ProjectSoft <projectsoft2009@yandex.ru>
2025-06-30 18:09:47 +0400

    update

---
+++ src/videoProviders/index.js
@@ -0,0 +1,13 @@
+const videoProviders = [
+	require("./aserPro"),
+	require("./rutube"),
+	require("./vk"),
+];
+
+exports.selectVideoProvider = function (url) {
+	for (let provider of videoProviders) {
+		if (provider.mayUse(url)) return provider;
+	}
+
+	throw new Error("Не найдено загрузчика для: " + url);
+};
