13f8ef76867268c77605b0bfe4c4aaf58c832a0f
ProjectSoft <projectsoft2009@yandex.ru>
2025-06-30 22:00:39 +0400

    Обновлена сборка url

--- src/videoProviders/aserPro.js
+++ src/videoProviders/aserPro.js
@@ -1,35 +1,35 @@
 const path = require("node:path");
+const emojiStrip = require('emoji-strip');
+const sanitize = require("sanitize-filename");
 const { downloadFile } = require("../downloadFile");
 const { selectVideoQuality } = require("../dialogue");
 const { uuid9 } = require("../uid");
 const { getManifest } = require("../m3u8Utils");
+
+const regexAserPro = /^https?:\/\/aser\.pro\/content\/.+?\/hls\/index.m3u8$/;
 
 module.exports = {
 	mayUse: url => regexAserPro.test(url),
 
 	loadVideo: async function (cfg) {
 		const videoInfo = await getManifest(cfg.url, "get video info:");
-		cfg.title = cfg.title ?? uuid9();
+		cfg.title = sanitize(emojiStrip(cfg.title ?? uuid9())).replace(/\s+/g, " ");
 		const [playlist, quality] = await selectVideoQuality(
 			cfg,
 			videoInfo["playlists"]
 		);
-		// Исправить вид url
-		// В windows path.join выставляет слеши в обратном направлении
-		const segmentsUrl = path.join(path.dirname(cfg.url), playlist);
 
+		const segmentsUrl = new URL(playlist, cfg.url).href;
 		const segmentsInfo = await getManifest(
 			segmentsUrl,
 			"get segments info:"
 		);
-		const segmentsBase = path.dirname(segmentsUrl);
+
 		const segmentsUrls = segmentsInfo["segments"].map(segment =>
-			path.join(segmentsBase, segment["uri"])
+			(new URL(segment["uri"], segmentsUrl)).href
 		);
 
 		const name = await downloadFile(cfg, segmentsUrls);
 		return [name, quality];
 	},
 };
-
-const regexAserPro = /^https?:\/\/aser\.pro\/content\/.+?\/hls\/index.m3u8$/;
